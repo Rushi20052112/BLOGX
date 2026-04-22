8789c40065483ee9287caa44ed5d6cb3ce9e303a
Rushi20052112 <rushikeshrepale746@gmail.com>
2026-04-23 01:00:15 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:3000","https://blogx-drab.vercel.app/"],
+    origin: ["http://localhost:3000","https://blogx-drab.vercel.app"],
     credentials: true
 }));
 
